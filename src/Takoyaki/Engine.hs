d57a0e81a52bd79c74249987512656ba3807c1b4
Fabien Boucher <fabien.dot.boucher@gmail.com>
2022-10-26 06:53:17 +0000

    First use of Ki

--- src/Takoyaki/Engine.hs
+++ src/Takoyaki/Engine.hs
@@ -7,12 +7,12 @@ module Takoyaki.Engine
   )
 where
 
-import Control.Concurrent.Async (concurrently_)
 import Control.Concurrent.STM
-import Control.Monad (forever)
+import Control.Monad (forever, void)
 import Control.Monad.IO.Class (liftIO)
 import Data.String.Interpolate (i)
 import Data.Text (Text)
+import qualified Ki
 import Lucid (Attribute, Html, ToHtml (toHtml), With (with), renderBS)
 import Lucid.Html5
 import Lucid.XStatic (xstaticScripts)
@@ -34,6 +34,14 @@ data App s se = App
     appService :: TVar s -> TBQueue se -> WS.Connection -> IO ()
   }
 
+kiConcurrently :: IO a -> IO b -> IO ()
+kiConcurrently a1 a2 =
+  Ki.scoped $ \scope -> do
+    t1 <- Ki.fork scope a1
+    void a2
+    void $ atomically (Ki.await t1)
+    pure ()
+
 connectionHandler :: App s se -> WS.Connection -> Handler ()
 connectionHandler app conn = liftIO $ do
   WS.withPingThread conn 5 (pure ()) $ do
@@ -43,7 +51,7 @@ connectionHandler app conn = liftIO $ do
     serviceQ <- newTBQueueIO 1
     appDom <- atomically $ app.appRender state
     WS.sendTextData conn . renderBS . div_ [id_ "init"] $ appDom
-    concurrently_ (handleEvents state serviceQ) (app.appService state serviceQ conn)
+    kiConcurrently (handleEvents state serviceQ) (app.appService state serviceQ conn)
   where
     handleEvents state serviceQ = forever $ do
       msg <- WS.receiveDataMessage conn
